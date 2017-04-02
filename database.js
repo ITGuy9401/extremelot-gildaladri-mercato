8aa9d330d7178cdfd1f930b94c0f895dae899491
Arcangelo Vicedomini <io@arcangelovicedomini.eu>
2017-04-02 13:09:42 +0200

    sequelize dev

--- database.js
+++ database.js
@@ -1,6 +1,38 @@
 (function(require, exports) {
-    'use strict';
+	'use strict';
 
-	 var Sequelize = require('sequelize');
-	 var 
+	var Sequelize = require('sequelize');
+	var connection = new Sequelize('extremelot_ladri', 'extremelot_ladri', null, {
+		host: 'localhost',
+		dialect: 'mysql',
+
+		pool: {
+			max: 5,
+			min: 0,
+			idle: 10000
+		}
+	});
+	connection.authenticate().complete(function(err) {
+		if (err) {
+			console.error('Unable to connect to the database:', err);
+		} else {
+			console.debug('Connection has been established successfully.');
+		}
+	});
+
+	var mapping = {};
+	mapping.prodotto = connection.define('prodotto', {
+		id: {
+			type: Sequelize.INTEGER,
+			autoIncrement: true
+		},
+		firstName: {
+			type: Sequelize.STRING,
+			field: 'first_name' // Will result in an attribute that is firstName when user facing but first_name in the database
+		},
+		lastName: {
+			type: Sequelize.STRING
+		}
+	});
+
 })(require, exports);
