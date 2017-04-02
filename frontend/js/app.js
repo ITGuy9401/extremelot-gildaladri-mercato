44f15464cfbab26e3d9a671a739fbd5a9c5011db
Arcangelo Vicedomini <io@arcangelovicedomini.eu>
2017-04-02 23:35:26 +0200

    bugfix

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -7,10 +7,11 @@ angular.module('mercatino', ['ngRoute', 'ui.bootstrap']).config(function($routeP
 		templateUrl: 'views/prodotti.html'
 	}).when('/admin', {
 		templateUrl: 'views/admin.html'
-	});
+	}).otherwise({
+		redirectTo: '/mercatino'
+	});;
 
-	// configure html5 to get links working on jsfiddle
-	$locationProvider.html5Mode(true);
 }).controller('menuCtrl', function($scope) {
 	var vm = this;
+}); = this;
 });
