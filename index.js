169c02288a6120c68de77bcf7f1468e8a1aa6ab7
Arcangelo Vicedomini <io@arcangelovicedomini.eu>
2017-04-02 15:40:47 +0200

    dev

--- index.js
+++ index.js
@@ -1,6 +1,46 @@
 const database = require('./database.js').database;
 const express = require('express');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+
 const app = express();
+
+passport.use(new LocalStrategy(
+	function(username, password, done) {
+		database.mapping.Utente.findOne({
+			username: username
+		}, function(err, user) {
+			if (err) {
+				return done(err);
+			}
+			if (!user) {
+				return done(null, false, {
+					message: 'Incorrect username.'
+				});
+			}
+			if (!user.validPassword(password)) {
+				return done(null, false, {
+					message: 'Incorrect password.'
+				});
+			}
+			return done(null, {
+				'id': user.id,
+				'lotname': user.lotname,
+				'username': user.username
+			});
+		});
+	}));
+
+
+app.post('/login',
+	passport.authenticate('local', {
+		successRedirect: '/',
+		failureRedirect: '/login',
+		failureFlash: true
+	})
+);
+
+
 require('./rest-product.js')(app, database);
 
 app.listen(4242, function() { // FIXME rendere variabile la porta
