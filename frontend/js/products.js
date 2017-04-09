64219c2c82f02189405ced4fd90c2353e09dd3c9
Arcangelo Vicedomini <io@arcangelovicedomini.eu>
2017-04-09 13:37:35 +0200

    dev

--- frontend/js/products.js
+++ frontend/js/products.js
@@ -1,3 +1,29 @@
-angular.module('mercatino').controller('productsCtrl', function($scope) {
+angular.module('mercatino').controller('productsCtrl', ['$http', function($http) {
 	var vm = this;
-});
+	vm.getListProducts = getListProducts;
+	vm.getListCategories = getListCategories;
+
+	function getListProducts() {
+		$http({
+			method: 'GET',
+			url: '/api/products'
+		}).then((response) => {
+			vm.prodotti = response.data;
+		}, (response) => {
+			alert.show("Errore nel recuperare l'elenco dei prodotti. Ricaricare la pagina.");
+			console.error("Errore nel recuperare l'elenco dei prodotti. Ricaricare la pagina.", response);
+		});
+	}
+
+	function getListCategories() {
+		$http({
+			method: 'GET',
+			url: '/api/categories'
+		}).then((response) => {
+			vm.categorie = response.data;
+		}, (response) => {
+			alert.show("Errore nel recuperare l'elenco delle categorie. Ricaricare la pagina.");
+			console.error("Errore nel recuperare l'elenco delle categorie. Ricaricare la pagina.", response);
+		});
+	}
+}]);
