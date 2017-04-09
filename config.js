17508401f04dc16f90592e4168abe8d21ff9e1c5
Arcangelo Vicedomini <io@arcangelovicedomini.eu>
2017-04-09 22:18:18 +0200

    dev

--- config.js
+++ config.js
@@ -1,6 +1,10 @@
 const nconf = require('nconf');
 
 // First consider commandline arguments and environment variables, respectively.
-nconf.argv().env();
+nconf.argv().env()
+/*.file({
+	file: 'config.json'
+});*/
+
 
 exports.nconf = nconf;
