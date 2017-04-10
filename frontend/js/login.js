135456842110ecad3b86feaa3d463d0cd1cbb1f5
Arcangelo Vicedomini <io@arcangelovicedomini.eu>
2017-04-10 22:36:31 +0200

    dev

--- frontend/js/login.js
+++ frontend/js/login.js
@@ -1,3 +1,21 @@
 angular.module('mercatino').controller('loginCtrl', function($scope) {
 	var vm = this;
+	vm.doSubmit = doSubmit;
+	vm.form = {};
+
+	function doSubmit() {
+		$http({
+			method: 'POST',
+			url: '/api/login',
+			data: {
+				'username': vm.form.username,
+				'password': vm.form.password
+			}
+		}).then((response) => {
+			console.log(JSON.stringify(response));
+		}, (response) => {
+			alert("Errore nella login.\n");
+			console.error("Errore nella login", response);
+		});
+	}
 });
