80e0a353ecfca3564806dda332ac9124145c9a43
Arcangelo Vicedomini <io@arcangelovicedomini.eu>
2017-04-10 13:27:26 +0200

    Update database.js

--- database.js
+++ database.js
@@ -12,6 +12,17 @@
 			max: Config.get('JAWSDB_POOL_MAX'),
 			min: Config.get('JAWSDB_POOL_MIN'),
 			idle: Config.get('JAWSDB_POOL_IDLE')
+		},
+		
+		define: {
+			// don't use camelcase for automatically added attributes but underscore style
+			// so updatedAt will be updated_at
+			underscored: true,
+
+			// disable the modification of tablenames; By default, sequelize will automatically
+			// transform all passed model names (first parameter of define) into plural.
+			// if you don't want that, set the following
+			freezeTableName: true
 		}
 	});
 	/*connection.authenticate().complete(function(err) {
