129ea73f8e0194cacfa275a2d489285db856a93d
Arcangelo Vicedomini <io@arcangelovicedomini.eu>
2017-04-17 03:01:09 +0200

    fix

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -1,48 +1,46 @@
-angular.module('mercatino', ['ngCookies', 'ngSanitize', 'ui.router', 'ui.bootstrap'])
-	.factory('loginInterceptor', function($q, $location) {
-		return {
-			response: function(response) {
-				// do something on success
-				return response;
-			},
-			responseError: function(response) {
-				if (response.status === 401) $location.url('/login');
-				else if (response.status === 403) alert('Non sei atorizzato ad eseguire questa azione.');
-				return $q.reject(response);
-			}
-		};
-	})
-	.config(function($httpProvider, $location, $stateProvider, $urlRouterProvider) {
-		$urlRouterProvider.otherwise('/mercatino');
+angular.module('mercatino', ['ngCookies', 'ngSanitize', 'ui.router', 'ui.bootstrap']).config(function($httpProvider, $location, $stateProvider, $urlRouterProvider) {
+	$urlRouterProvider.otherwise('/mercatino');
 
-		$stateProvider.state('login', {
-			url: '/login',
-			templateUrl: 'views/login.html',
-			controller: 'loginCtrl',
-			controllerAs: 'vm'
-		}).state('mercatino', {
-			url: '/mercatino',
-			templateUrl: 'views/mercatino.html'
-		}).state('regolamento', {
-			url: '/regolamento',
-			templateUrl: 'views/regolamento.html'
-		}).state('prodotti', {
-			url: '/prodotti',
-			templateUrl: 'views/prodotti.html',
-			controller: 'productsCtrl',
-			controllerAs: 'vm'
-		}).state('prodotto', {
-			url: '/prodotto/:productCode',
-			templateUrl: 'views/prodotto.html',
-			controller: 'productCtrl',
-			controllerAs: 'vm'
-		}).state('admin', {
-			url: '/admin',
-			templateUrl: 'views/admin.html',
-			controller: 'adminCtrl',
-			controllerAs: 'vm'
-		});
-		$httpProvider.interceptors.push('loginInterceptor');
-	}).controller('menuCtrl', function($scope) {
-		var vm = this;
+	$stateProvider.state('login', {
+		url: '/login',
+		templateUrl: 'views/login.html',
+		controller: 'loginCtrl',
+		controllerAs: 'vm'
+	}).state('mercatino', {
+		url: '/mercatino',
+		templateUrl: 'views/mercatino.html'
+	}).state('regolamento', {
+		url: '/regolamento',
+		templateUrl: 'views/regolamento.html'
+	}).state('prodotti', {
+		url: '/prodotti',
+		templateUrl: 'views/prodotti.html',
+		controller: 'productsCtrl',
+		controllerAs: 'vm'
+	}).state('prodotto', {
+		url: '/prodotto/:productCode',
+		templateUrl: 'views/prodotto.html',
+		controller: 'productCtrl',
+		controllerAs: 'vm'
+	}).state('admin', {
+		url: '/admin',
+		templateUrl: 'views/admin.html',
+		controller: 'adminCtrl',
+		controllerAs: 'vm'
 	});
+	$httpProvider.interceptors.push('loginInterceptor');
+}).factory('loginInterceptor', function($q, $location) {
+	return {
+		response: function(response) {
+			// do something on success
+			return response;
+		},
+		responseError: function(response) {
+			if (response.status === 401) $location.url('/login');
+			else if (response.status === 403) alert('Non sei atorizzato ad eseguire questa azione.');
+			return $q.reject(response);
+		}
+	};
+}).controller('menuCtrl', function($scope) {
+	var vm = this;
+});
