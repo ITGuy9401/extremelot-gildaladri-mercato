6e4fe9f385c55c0cdcaaac980b8107782f66eae3
Arcangelo Vicedomini <io@arcangelovicedomini.eu>
2017-04-17 02:56:14 +0200

    fix

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -1,36 +1,5 @@
-angular.module('mercatino', ['ngCookies', 'ngSanitize', 'ui.router', 'ui.bootstrap']).config(function($stateProvider, $urlRouterProvider) {
-	$urlRouterProvider.otherwise('/mercatino');
-
-	$stateProvider.state('login', {
-		url: '/login',
-		templateUrl: 'views/login.html',
-		controller: 'loginCtrl',
-		controllerAs: 'vm'
-	}).state('mercatino', {
-		url: '/mercatino',
-		templateUrl: 'views/mercatino.html'
-	}).state('regolamento', {
-		url: '/regolamento',
-		templateUrl: 'views/regolamento.html'
-	}).state('prodotti', {
-		url: '/prodotti',
-		templateUrl: 'views/prodotti.html',
-		controller: 'productsCtrl',
-		controllerAs: 'vm'
-	}).state('prodotto', {
-		url: '/prodotto/:productCode',
-		templateUrl: 'views/prodotto.html',
-		controller: 'productCtrl',
-		controllerAs: 'vm'
-	}).state('admin', {
-		url: '/admin',
-		templateUrl: 'views/admin.html',
-		controller: 'adminCtrl',
-		controllerAs: 'vm'
-	});
-
-}).run(['$httpProvider', '$location', ($httpProvider, $location) => {
-	$httpProvider.interceptors.push(function($q, $location) {
+angular.module('mercatino', ['ngCookies', 'ngSanitize', 'ui.router', 'ui.bootstrap'])
+	.factory('loginInterceptor', function($q, $location) {
 		return {
 			response: function(response) {
 				// do something on success
@@ -42,7 +11,38 @@ angular.module('mercatino', ['ngCookies', 'ngSanitize', 'ui.router', 'ui.bootstr
 				return $q.reject(response);
 			}
 		};
+	})
+	.config(function($httpProvider, $location, $stateProvider, $urlRouterProvider) {
+		$urlRouterProvider.otherwise('/mercatino');
+
+		$stateProvider.state('login', {
+			url: '/login',
+			templateUrl: 'views/login.html',
+			controller: 'loginCtrl',
+			controllerAs: 'vm'
+		}).state('mercatino', {
+			url: '/mercatino',
+			templateUrl: 'views/mercatino.html'
+		}).state('regolamento', {
+			url: '/regolamento',
+			templateUrl: 'views/regolamento.html'
+		}).state('prodotti', {
+			url: '/prodotti',
+			templateUrl: 'views/prodotti.html',
+			controller: 'productsCtrl',
+			controllerAs: 'vm'
+		}).state('prodotto', {
+			url: '/prodotto/:productCode',
+			templateUrl: 'views/prodotto.html',
+			controller: 'productCtrl',
+			controllerAs: 'vm'
+		}).state('admin', {
+			url: '/admin',
+			templateUrl: 'views/admin.html',
+			controller: 'adminCtrl',
+			controllerAs: 'vm'
+		});
+		$httpProvider.interceptors.push('loginInterceptor');
+	}).controller('menuCtrl', function($scope) {
+		var vm = this;
 	});
-}]).controller('menuCtrl', function($scope) {
-	var vm = this;
-});
